ef09d7d3dd192aa42f4079b06f2ea78f1aef57fe
Lorena Cueva <lorenacueva@gmail.com>
2023-01-23 10:53:43 -0800

    commit tree 08481d68659ddb8901e37035474024f2758f171f

--- lib/balancing_parentheses.rb
+++ lib/balancing_parentheses.rb
@@ -1,3 +1,18 @@
 require_relative './stack'
 
-# your code here
+def balancing_parentheses(input)
+    stack = Stack.new
+
+    def balancing_parentheses(input)
+        stack = Stack.new
+    
+        input.chars.each do |e|
+            if stack.peek == '(' && e == ')'
+                stack.pop
+            else
+                stack.push(e)
+            end
+        end
+        stack.size 
+    end
+end    
